98d7858edfd4c8160fadee17d0f6b474dd01aacd
Tim Berry <timothyaberry@gmail.com>
2015-11-02 08:41:55 -0700

    Initial upload

---
+++ js/teams/teamCtrl.js
@@ -0,0 +1,8 @@
+var app = angular.module('nbaRoutes');
+
+// this controller needs to be injected the resolved data from the router
+app.controller('teamCtrl', function ($scope, $stateParams, teamService) {
+
+    // controller code
+
+});
